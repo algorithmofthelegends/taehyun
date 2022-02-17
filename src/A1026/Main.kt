68a447085d2eeab695abe93d350c1e28b0a1ce6b
hare <xogus0790@naver.com>
2022-02-17 19:05:02 +0900

    update 1026

--- src/A1026/Main.kt
+++ src/A1026/Main.kt
@@ -2,28 +2,20 @@ package A1026
 
 fun main() {
     val n = readLine()!!.toInt()
-    val a = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    val a = readLine()!!.split(" ").map { it.toInt() }.sorted()
     val b = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
     var result = 0
 
-    for (i in 1..n) {
-        var idxA = 0;
-        for (x in a.indices) {
-            if (a[x] < a[idxA]) {
-                idxA = x
-            }
-        }
-
-        var idxB = 0;
+    for (i in 0 until n) {
+        var idx = 0;
         for (y in b.indices) {
-            if (b[y] > b[idxB]) {
-                idxB = y
+            if (b[y] > b[idx]) {
+                idx = y
             }
         }
 
-        result += a[idxA] * b[idxB]
-        a[idxA] = 101
-        b[idxB] = -1
+        result += a[i] * b[idx]
+        b[idx] = -1
     }
     print(result)
 }
